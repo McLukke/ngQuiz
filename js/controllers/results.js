ea2b8a941198d3e527b389e1c9efa64953cd1625
Kenny Lu <kenny@whynotkenny.com>
2017-07-30 17:21:04 -0400

    controller logic for showing results and styles

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -10,5 +10,15 @@
 
     vm.quizMetrics = quizMetrics;
     vm.dataService = dataService;
+    vm.activeQuestion = 0;
+    vm.getAnswerClass = getAnswerClass;
+
+    function getAnswerClass(index) {
+      if (index === quizMetrics.correctAnswers[vm.activeQuestion]) {
+        return 'bg-success';
+      } else if (index === dataService.quizQuestions[vm.activeQuestion].selected) {
+        return 'bg-danger';
+      }
+    }
   }
 })();
