fdb7530a6d91461d93a5ea8bce9805a5c225d34d
Kenny Lu <kenny@whynotkenny.com>
2017-07-30 13:13:45 -0400

    add data service to acquire data / inject data

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -3,13 +3,13 @@
     .module("turtleFacts")
     .controller("quizController", QuizController);
 
-  QuizController.$inject = ['quizMetrics'];
+  QuizController.$inject = ['quizMetrics', 'dataService'];
 
-  function QuizController(quizMetrics) {
+  function QuizController(quizMetrics, dataService) {
     var vm = this;
 
     vm.quizMetrics = quizMetrics;
   }
 
 
-})()
+})();
