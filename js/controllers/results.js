8e8b54618fa2a7c02af2ea09bc8bbf36e6457d5d
Kenny Lu <kenny@whynotkenny.com>
2017-07-30 17:28:58 -0400

    display score area / calculate score percentage / format score

--- js/controllers/results.js
+++ js/controllers/results.js
@@ -12,6 +12,8 @@
     vm.dataService = dataService;
     vm.activeQuestion = 0;
     vm.getAnswerClass = getAnswerClass;
+    vm.setActiveQuestion = setActiveQuestion;
+    vm.calculatePerc = calculatePerc;
 
     function getAnswerClass(index) {
       if (index === quizMetrics.correctAnswers[vm.activeQuestion]) {
@@ -19,6 +21,14 @@
       } else if (index === dataService.quizQuestions[vm.activeQuestion].selected) {
         return 'bg-danger';
       }
+    }
+
+    function setAnswerClass(index) {
+      vm.activeQuestion = index;
+    }
+
+    function calculatePerc() {
+      return quizMetrics.numCorrect / dataService.quizQuestions.length * 100;
     }
   }
 })();
