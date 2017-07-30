b1738f2f6149e79c6a8c987b73b6ca801676c5a4
Kenny Lu <kenny@whynotkenny.com>
2017-07-30 15:03:30 -0400

    iterating through questions and show completed/answered states vs selected states

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -12,11 +12,39 @@
     vm.dataService = dataService;
     vm.activeQuestion = 0;
     vm.questionAnswered = questionAnswered;
+    vm.setActiveQuestion = setActiveQuestion;
+
+    var numQuestionsAnswered = 0;
 
     function questionAnswered() {
-      
+      var quizLength = dataService.quizQuestions.length;
+
+      if (dataService.quizQuestions[vm.activeQuestion].selected !== null) {
+        numQuestionsAnswered++;
+        if (numQuestionsAnswered >= quizLength) {
+          // finalize quiz
+        }
+      }
+
+      vm.setActiveQuestion();
+    }
+
+    function setActiveQuestion() {
+      var breakOut = false;
+      var quizLength = dataService.quizQuestions.length - 1;
+
+      while(!breakOut) {
+        if (vm.activeQuestion < quizLength) {
+          vm.activeQuestion = ++vm.activeQuestion;
+        } else {
+          vm.activeQuestion = 0;
+        }
+        console.log('vm.activeQuestion: ', vm.activeQuestion);
+
+        if (dataService.quizQuestions[vm.activeQuestion].selected === null) {
+          breakOut = true;
+        }
+      }
     }
   }
-
-
 })();
